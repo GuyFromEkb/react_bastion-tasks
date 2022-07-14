6ef9e688658f286f7fc17397ecddc1df2b84585d
Sergey <graygmai1@gmail.com>
2022-07-15 03:26:32 +0500

    header

---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import "./header.scss"
+import Nav from "../nav/Nav"
+import HeaderSearch from "../headerSearch/HeaderSearch"
+
+function Header() {
+
+
+    return (
+        <header>
+            <Nav />
+            <HeaderSearch />
+
+        </header>
+    )
+
+}
+
+
+export default Header
