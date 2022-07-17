d2890c5a1aaac6f2e1c451c194f92161f9a90249
Sergey <graygmai1@gmail.com>
2022-07-17 12:02:34 +0500

    filter Gost

--- src/components/itemList/ItemList.js
+++ src/components/itemList/ItemList.js
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux'
-import { allProduct } from '../../store/porduct/productSelector'
+import { allProduct, viewProduct } from '../../store/porduct/productSelector'
+import { allgostFilter } from '../../store/gostFilter/gostFilterSelector'
 
 import './itemList.scss'
 import Item from '../item/Item'
@@ -9,9 +10,17 @@ import Item from '../item/Item'
 
 const ItemList = () => {
 
-    const itemData = useSelector(allProduct)
-    console.log(itemData);
 
+    // const itemData = useSelector(allProduct)
+    const activeFilterData = useSelector(allgostFilter)
+    // console.log(itemData);
+    // console.log(activeFilterData);
+
+    // const buff = useSelector((state) => viewProduct(state, activeFilterData))
+
+
+    const itemData = useSelector((state) => viewProduct(state, activeFilterData))
+    // console.log(buff);
 
     return (
         <div className="item-list">
--- src/store/porduct/productSelector.js
+++ src/store/porduct/productSelector.js
@@ -1,3 +1,22 @@
 const allProduct = (state) => state.product
 
-export { allProduct }
+const viewProduct = (state, filter = []) => {
+
+    if (filter.length === 0) return state.product
+
+    return state.product.filter(item => {
+
+        const productGost = [...item.gost]
+
+        /* const a =  */
+        return filter.some(filterItem => productGost.includes(filterItem))
+
+        // console.log(`filter = `, filter);
+        // console.log(productGost, '==================', a);
+
+    })
+
+    // return state.product
+}
+
+export { allProduct, viewProduct }
