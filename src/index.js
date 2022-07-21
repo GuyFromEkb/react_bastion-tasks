15f432dcb3b7bb737ef6262c45fbd835c02a39cd
Sergey <graygmai1@gmail.com>
2022-07-21 09:48:41 +0500

    strick mode

--- src/index.js
+++ src/index.js
@@ -10,13 +10,11 @@ import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-
+  <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-      <App />
+        <App />
       </Provider>
     </BrowserRouter>
-
-  // </React.StrictMode>
+  </React.StrictMode>
 );
